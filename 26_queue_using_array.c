45ec1fd01dc8ee520c18b3fc3def7b860f4c34db
ceccs17d43 <42961666+ceccs17d43@users.noreply.github.com>
2018-11-30 15:46:34 +0530

    Update 26_queue_using_array.c

--- 26_queue_using_array.c
+++ 26_queue_using_array.c
@@ -1,125 +1,125 @@
-//NAME        :ROSHIN ROY
-//ROLL NO     :43
-//PROGRAM NO  :26
-//PROGRAM NAME:QUEUE USING ARRAY
+	//NAME        :ROSHIN ROY
+	//ROLL NO     :43
+	//PROGRAM NO  :26
+	//PROGRAM NAME:QUEUE USING ARRAY
 
-#include <stdio.h>
+	#include <stdio.h>
 
-int queue[100], n=100, front=-1, rear=-1;
+	int queue[100], n=100, front=-1, rear=-1;
 
-void enqueue() {
-	int data;
-	if (rear == n-1) {
-		printf("Failed: Queue full\n");
-	} else {
-		printf("Enter data to be entered : ");
-		scanf("%d", &data);
+	void enqueue() {
+		int data;
+		if (rear == n-1) {
+			printf("Failed: Queue full\n");
+		} else {
+			printf("Enter data to be entered : ");
+			scanf("%d", &data);
+			if (front == -1) {
+				front = rear = 0;
+			} else {
+				rear++;
+			}
+			queue[rear] = data;
+		}
+	}
+	void dequeue() {
 		if (front == -1) {
-			front = rear = 0;
+			printf("Failed: Empty queue\n");
 		} else {
-			rear++;
+			printf("Deleted element : %d\n", queue[front]);
+			if (front == rear) {
+				front = rear = -1;
+			} else {
+				front++;
+			}
 		}
-		queue[rear] = data;
 	}
-}
-void dequeue() {
-	if (front == -1) {
-		printf("Failed: Empty queue\n");
-	} else {
-		printf("Deleted element : %d\n", queue[front]);
-		if (front == rear) {
-			front = rear = -1;
+	void printQueue() {
+		int i=0;
+		if (front == -1) {
+			printf("Empty Queue\n");
 		} else {
-			front++;
+			for (i=front; i<=rear; i++) {
+				printf("%d\n", queue[i]);
+			}
 		}
 	}
-}
-void printQueue() {
-	int i=0;
-	if (front == -1) {
-		printf("Empty Queue\n");
-	} else {
-		for (i=front; i<=rear; i++) {
-			printf("%d\n", queue[i]);
-		}
+	int main() {
+		char choice;
+		front = rear = -1;
+		do {
+			printf("\n\tQueue using array\n1.Enqueu\n2.Dequeue\n3.Display\n4.Exit\nEnter your choice : ");
+			scanf(" %c", &choice);
+			switch(choice) {
+				case '1':
+					enqueue();
+					break;
+				case '2':
+					dequeue();
+					break;
+				case '3':
+					printQueue();
+					break;
+			}
+		} while(choice != '4');
 	}
-}
-int main() {
-	char choice;
-	front = rear = -1;
-	do {
-		printf("\n\tQueue using array\n1.Enqueu\n2.Dequeue\n3.Display\n4.Exit\nEnter your choice : ");
-		scanf(" %c", &choice);
-		switch(choice) {
-			case '1':
-				enqueue();
-				break;
-			case '2':
-				dequeue();
-				break;
-			case '3':
-				printQueue();
-				break;
-		}
-	} while(choice != '4');
-}
 
-OUTPUT :
+	OUTPUT :
 
 
-        Queue using array
-1.Enqueu
-2.Dequeue
-3.Display
-4.Exit
-Enter your choice : 1
-Enter data to be entered : 1
+		Queue using array
+	1.Enqueu
+	2.Dequeue
+	3.Display
+	4.Exit
+	Enter your choice : 1
+	Enter data to be entered : 1
 
-        Queue using array
-1.Enqueu
-2.Dequeue
-3.Display
-4.Exit
-Enter your choice : 1
-Enter data to be entered : 2
+		Queue using array
+	1.Enqueu
+	2.Dequeue
+	3.Display
+	4.Exit
+	Enter your choice : 1
+	Enter data to be entered : 2
 
-        Queue using array
-1.Enqueu
-2.Dequeue
-3.Display
-4.Exit
-Enter your choice : 1
-Enter data to be entered : 3
+		Queue using array
+	1.Enqueu
+	2.Dequeue
+	3.Display
+	4.Exit
+	Enter your choice : 1
+	Enter data to be entered : 3
 
-        Queue using array
-1.Enqueu
-2.Dequeue
-3.Display
-4.Exit
-Enter your choice : 1
-Enter data to be entered : 5
+		Queue using array
+	1.Enqueu
+	2.Dequeue
+	3.Display
+	4.Exit
+	Enter your choice : 1
+	Enter data to be entered : 5
 
-        Queue using array
-1.Enqueu
-2.Dequeue
-3.Display
-4.Exit
-Enter your choice : 2
-Deleted element : 1
+		Queue using array
+	1.Enqueu
+	2.Dequeue
+	3.Display
+	4.Exit
+	Enter your choice : 2
+	Deleted element : 1
 
-        Queue using array
-1.Enqueu
-2.Dequeue
-3.Display
-4.Exit
-Enter your choice : 3
-2
-3
-5
+		Queue using array
+	1.Enqueu
+	2.Dequeue
+	3.Display
+	4.Exit
+	Enter your choice : 3
+	2
+	3
+	5
 
-        Queue using array
-1.Enqueu
-2.Dequeue
-3.Display
-4.Exit
-Enter your choice : 4
+		Queue using array
+	1.Enqueu
+	2.Dequeue
+	3.Display
+	4.Exit
+	Enter your choice : 4
