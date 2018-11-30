3c03400ed0db18635cbec0523272a3e6f649e0bf
ceccs17d43 <42961666+ceccs17d43@users.noreply.github.com>
2018-11-30 11:42:34 +0530

    Update 04_bubblesort.c

--- 04_bubblesort.c
+++ 04_bubblesort.c
@@ -1,32 +1,55 @@
-#include <stdio.h>
+	//NAME        :ROSHIN ROY
+	//ROLL NO     :43
+	//PROGRAM NO  :1
+	//PROGRAM NAME:SEQUENTIAL SEARCH
 
-int bubbleSort(int array[], int size) {
-	int i, j, temp;
-	for (i=size; i>=0; i--) {
-		for (j=0; j<i; j++) {
-			if (array[j] > array[j+1]) {
-				temp = array[j];
-				array[j] = array[j+1];
-				array[j+1] = temp;
+	#include <stdio.h>
+
+	int bubbleSort(int array[], int size) {
+		int i, j, temp;
+		for (i=size; i>=0; i--) {
+			for (j=0; j<i; j++) {
+				if (array[j] > array[j+1]) {
+					temp = array[j];
+					array[j] = array[j+1];
+					array[j+1] = temp;
+				}
 			}
 		}
 	}
-}
-int main() {
-	int i, j, array[100], size, temp;
+	int main() {
+		int i, j, array[100], size, temp;
 
-	printf("Enter number of elements in the array : ");
-	scanf("%d", &size);
+		printf("Enter number of elements in the array : ");
+		scanf("%d", &size);
 
-	printf("Enter the array\n");
-	for (i=0; i<size; i++) {
-		scanf("%d", &array[i]);
+		printf("Enter the array\n");
+		for (i=0; i<size; i++) {
+			scanf("%d", &array[i]);
+		}
+
+		bubbleSort(array, size);
+
+		printf("Sorted array \n");
+		for (i=0; i<size; i++) {
+			printf("%d\n", array[i]);
+		}
 	}
 
-	bubbleSort(array, size);
+	OUTPUT:
 
-	printf("Sorted array \n");
-	for (i=0; i<size; i++) {
-		printf("%d\n", array[i]);
-	}
-}
+
+		Enter number of elements in the array : 4                                                 
+		Enter the array                                                                           
+		3                                                                                         
+		4                                                                                         
+		2                                                                                         
+		7                                                                                         
+		Sorted array                                                                              
+		2                                                                                         
+		3                                                                                         
+		4                                                                                         
+		7                                                                                         
+           
+
+	
