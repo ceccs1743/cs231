d6e44476d11c49e404382aad4bb80f12014f8442
ceccs17d43 <42961666+ceccs17d43@users.noreply.github.com>
2018-11-30 15:57:56 +0530

    Update 08_quicksort_recursive.c

--- 08_quicksort_recursive.c
+++ 08_quicksort_recursive.c
@@ -1,66 +1,66 @@
-//NAME        :ROSHIN ROY
-//ROLL NO     :43
-//PROGRAM NO  :8
-//PROGRAM NAME:QUICKSORT:RECURSIVE
-#include <stdio.h>
+	//NAME        :ROSHIN ROY
+	//ROLL NO     :43
+	//PROGRAM NO  :8
+	//PROGRAM NAME:QUICKSORT:RECURSIVE
+	#include <stdio.h>
 
-void swap(int *a, int *b) {
-	int temp = *a;
-	*a = *b;
-	*b = temp;
-}
-int partition(int array[], int low, int high) {
-	int i=low;
-	int j=high;
-	int pivot=array[low];
-	while (1) {
-		while (array[i] < pivot)
-			i++;
-		while (array[j] > pivot)
-			j--;
-		if (i>=j)
-			return j;
-		swap(&array[i], &array[j]);
+	void swap(int *a, int *b) {
+		int temp = *a;
+		*a = *b;
+		*b = temp;
 	}
-}
-void quickSort(int array[], int low, int high) {
-	int partitionIndex;
-	if (low < high) {
-		partitionIndex = partition(array, low, high);
-		quickSort(array, low, partitionIndex);
-		quickSort(array, partitionIndex+1, high);
+	int partition(int array[], int low, int high) {
+		int i=low;
+		int j=high;
+		int pivot=array[low];
+		while (1) {
+			while (array[i] < pivot)
+				i++;
+			while (array[j] > pivot)
+				j--;
+			if (i>=j)
+				return j;
+			swap(&array[i], &array[j]);
+		}
 	}
-}
-int printArray(int a[], int size) {
-	int i=0;
-	for (i=0; i<size; i++) {
-		printf("%d\n", a[i]);
+	void quickSort(int array[], int low, int high) {
+		int partitionIndex;
+		if (low < high) {
+			partitionIndex = partition(array, low, high);
+			quickSort(array, low, partitionIndex);
+			quickSort(array, partitionIndex+1, high);
+		}
 	}
-}
-int main() {
-	int array[100], i, size;
-	printf("Enter size of the array : ");
-	scanf("%d", &size);
-	printf("Enter array elements\n");
-	for (i=0; i<size; i++)
-		scanf("%d", &array[i]);
-	printf("Sorted Array\n");
-	quickSort(array, 0, size-1);
-	printArray(array, size);
-}
+	int printArray(int a[], int size) {
+		int i=0;
+		for (i=0; i<size; i++) {
+			printf("%d\n", a[i]);
+		}
+	}
+	int main() {
+		int array[100], i, size;
+		printf("Enter size of the array : ");
+		scanf("%d", &size);
+		printf("Enter array elements\n");
+		for (i=0; i<size; i++)
+			scanf("%d", &array[i]);
+		printf("Sorted Array\n");
+		quickSort(array, 0, size-1);
+		printArray(array, size);
+	}
 
-OUTPUT :
+	OUTPUT :
 
-Enter size of the array : 5
-Enter array elements
-16
-14
-3
-9
-21
-Sorted Array
-3
-9
-14
-16
-21
+	Enter size of the array : 5
+	Enter array elements
+	16
+	14
+	3
+	9
+	21
+	Sorted Array
+	3
+	9
+	14
+	16
+	21
