8b81cfec6f26c98841d06d8614e35a260404be29
ceccs17d43 <42961666+ceccs17d43@users.noreply.github.com>
2018-11-30 15:56:51 +0530

    Update 10_binarysearch.c

--- 10_binarysearch.c
+++ 10_binarysearch.c
@@ -1,84 +1,84 @@
 
-//NAME        :ROSHIN ROY
-//ROLL NO     :43
-//PROGRAM NO  :10
-//PROGRAM NAME:BINARYSEARCH
+	//NAME        :ROSHIN ROY
+	//ROLL NO     :43
+	//PROGRAM NO  :10
+	//PROGRAM NAME:BINARYSEARCH
 
-#include <stdio.h>
+	#include <stdio.h>
 
-int selectionSort(int array[], int size) {
-	int i, j, pos, temp;
-	for (i=0; i<size; i++) {
-		pos = i;
+	int selectionSort(int array[], int size) {
+		int i, j, pos, temp;
+		for (i=0; i<size; i++) {
+			pos = i;
 
-		for (j=i; j<size; j++) {
-			if (array[j] < array[pos]) {
-				pos =j;
+			for (j=i; j<size; j++) {
+				if (array[j] < array[pos]) {
+					pos =j;
+				}
+			}
+			if (i != pos) {
+				temp = array[i];
+				array[i] = array[pos];
+				array[pos] = temp;
 			}
 		}
-		if (i != pos) {
-			temp = array[i];
-			array[i] = array[pos];
-			array[pos] = temp;
+	}
+
+	int main() {
+		int i, beg, mid, last, found=0, array[100], element, size;
+
+		printf("Enter number of elements : ");
+		scanf("%d", &size);
+
+		printf("Enter elements\n");
+		for (i=0; i<size; i++) {
+			scanf("%d", &array[i]);
 		}
-	}
-}
 
-int main() {
-	int i, beg, mid, last, found=0, array[100], element, size;
+		selectionSort(array, size);
 
-	printf("Enter number of elements : ");
-	scanf("%d", &size);
+		printf("Sorted array\n");
+		for (i=0; i<size; i++) {
+			printf("%d\n", array[i]);
+		}
+		printf("Enter element to search : ");
+		scanf("%d", &element);
 
-	printf("Enter elements\n");
-	for (i=0; i<size; i++) {
-		scanf("%d", &array[i]);
-	}
+		beg = 0;
+		last = size-1;
+		while (last >= beg) {
+			mid = (beg + last)/2;
+			if (array[mid] == element) {
+				found = 1;
+				break;
+			} else if (array[mid] > element) {
+				last = mid - 1;
+			} else {
+				beg = mid + 1;
+			}
+		}
 
-	selectionSort(array, size);
-
-	printf("Sorted array\n");
-	for (i=0; i<size; i++) {
-		printf("%d\n", array[i]);
-	}
-	printf("Enter element to search : ");
-	scanf("%d", &element);
-
-	beg = 0;
-	last = size-1;
-	while (last >= beg) {
-		mid = (beg + last)/2;
-		if (array[mid] == element) {
-			found = 1;
-			break;
-		} else if (array[mid] > element) {
-			last = mid - 1;
+		if (found) {
+			printf("Element found at position %d\n", mid+1);
 		} else {
-			beg = mid + 1;
+			printf("Element not found\n");
 		}
 	}
 
-	if (found) {
-		printf("Element found at position %d\n", mid+1);
-	} else {
-		printf("Element not found\n");
-	}
-}
+	OUTPUT :
 
-OUTPUT :
-
-Enter number of elements : 5
-Enter elements
-12
-45
-17
-18
-23
-Sorted array
-12
-17
-18
-23
-45
-Enter element to search : 23
-Element found at position 4
+	Enter number of elements : 5
+	Enter elements
+	12
+	45
+	17
+	18
+	23
+	Sorted array
+	12
+	17
+	18
+	23
+	45
+	Enter element to search : 23
+	Element found at position 4
